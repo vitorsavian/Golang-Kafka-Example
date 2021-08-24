a361903538a69c2aeb8219a64382ac77067d2115
Vitor <vitorandresaviandev@gmail.com>
2021-08-24 20:51:56 -0300

    Creating listener for kafka sync method

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("mensagem", "teste", producer, nil)
+	Publish("mensagem", "teste", producer, nil, deliveryChan)
+
+	e := <-deliveryChan
+
+	msg := e.(*kafka.Message)
+
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Erro ao enviar")
+	} else {
+		fmt.Println("Mensagem enviada", msg.TopicPartition)
+	}
+
 	producer.Flush(1000)
 }
 
@@ -26,7 +39,7 @@ func NewKafkaProducer() *kafka.Producer {
 	return producer
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChannel chan kafka.Event) error {
 	messsage := &kafka.Message{
 		Value: []byte(msg),
 		TopicPartition: kafka.TopicPartition{
@@ -35,6 +48,6 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte) err
 		},
 		Key: key,
 	}
-	err := producer.Produce(messsage, nil)
+	err := producer.Produce(messsage, deliveryChannel)
 	return err
 }
