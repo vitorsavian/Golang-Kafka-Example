348a1e0e4ecd95e54fcc23da8180745aa80f4e50
Vitor <vitorandresaviandev@gmail.com>
2021-08-24 19:01:59 -0300

    Producer created and setup done

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
 	fmt.Println("Hello Go")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "golang-kafka-example_kafka_1:9092",
+	}
+
+	producer, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return producer
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iconicsoda/Golang-Kafka-Example
 
 go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
